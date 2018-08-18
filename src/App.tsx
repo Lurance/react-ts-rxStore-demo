60f7ff1bdcd1c8ab3d9e75aaad918c480c785fa0
Lurance <353650263@qq.com>
2018-08-18 11:14:15 +0800

    feat: add async http render demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { autobind } from 'core-decorators';
+import Axios from 'axios';
 import appStoreFactory from './store/appStoreFactory';
 import observer from './store/observer';
 
 @autobind()
-class App extends React.Component<{ onHeaderTabsAdd: any, headerTabs: string[] }> {
+class App extends React.Component<{ onHeaderTabsAdd: any, onHeaderTabsSet:any, headerTabs: string[] }> {
   constructor(args) {
     super(args);
 
     setInterval(() => {
       this.handleHeaderTabsAdd('Tabs rendered from Intervaler')
     }, 2000)
+  }
+
+  public componentDidMount() {
+    Axios.get('https://sammy-1252377356.cos.ap-beijing.myqcloud.com/header-config.json')
+      .then(res => this.props.onHeaderTabsSet(res.data))
   }
 
   public componentDidUpdate(preProps) {
@@ -34,10 +40,10 @@ class App extends React.Component<{ onHeaderTabsAdd: any, headerTabs: string[] }
 }
 
 
-
 export default observer(App,
   appStoreFactory,
   {
-    onHeaderTabsAdd: (v) => (state) => state.headerTabs.push(v)
+    onHeaderTabsAdd: (v) => (state) => state.headerTabs.push(v),
+    onHeaderTabsSet: (v) => (state) => state.headerTabs = [...v]
   }
 );
